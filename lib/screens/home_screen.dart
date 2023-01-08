f3c8d3464f50731556fe964ff5572f7d59f9db0c
Willian Rosa <willyrosaramirezc1@gmail.com>
2023-01-08 11:57:55 -0600

    saving changes, january 8 of 2023

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/providers/movies_provider.dart';
+import 'package:peliculas/widgets/widgets.dart';
+import 'package:provider/provider.dart';
+
+import 'package:peliculas/search/search_delegate.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final moviesProvider = Provider.of<MoviesProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Peliculas en cines'),
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () => showSearch(
+              context: context, 
+              delegate: MovieSearchDelegate()
+            ),
+            icon: Icon(Icons.search_outlined))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // CardSwiper Tarjetas principales
+            CardSwiper( movies:moviesProvider.onDisplayMovies ),
+      
+            // Listado horizontal de películas
+            MovieSlider(
+              movies:moviesProvider.popularMovies,
+              title:'Populares!',
+              onNextPage: ()=> moviesProvider.getPopularMovies(),
+            )
+      
+          ],
+        ),
+      )
+    );
+  }
+}
